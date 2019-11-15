e0391e5a670f3525028043b0463aee181f2e704b
Ahmad Syamim <ahmad.syamim@gmail.com>
2019-11-15 15:18:47 +0900

    fixed front-end menu

--- resources/views/vendor/voyager-frontend/partials/menu-left.blade.php
+++ resources/views/vendor/voyager-frontend/partials/menu-left.blade.php
@@ -1,7 +1,7 @@
 @foreach($items as $menu_item)
     @php ($hasChildren = count($menu_item->children) > 0)
     <li>
-        <a href="{{ $menu_item->link() }}">{{ $menu_item->title }}</a>
+        <a href="{{ url($menu_item->link()) }}">{{ $menu_item->title }}</a>
         @if ($hasChildren)
             <ul class="menu">
                 @include('voyager-frontend::partials.menu-left', ['items' => $menu_item->children])
