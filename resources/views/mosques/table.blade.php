5ed6abbe7e05aea0a305bb6662c49031f0b51422
Ahmad Syamim <ahmad.syamim@gmail.com>
2019-11-20 11:20:44 +0900

    update mosques/table.blade.php

--- resources/views/mosques/table.blade.php
+++ resources/views/mosques/table.blade.php
@@ -16,23 +16,14 @@
         <tbody>
         @foreach($mosques as $mosque)
             <tr>
-                <td>{!! $mosque->name !!}</td>
-            <td>{!! $mosque->image !!}</td>
-            <td>{!! $mosque->description !!}</td>
-            <td>{!! $mosque->address !!}</td>
-            <td>{!! $mosque->website !!}</td>
-            <td>{!! $mosque->prefectures !!}</td>
-            <td>{!! $mosque->city !!}</td>
-            <td>{!! $mosque->status !!}</td>
-                <td>
-                    {!! Form::open(['route' => ['mosques.destroy', $mosque->id], 'method' => 'delete']) !!}
-                    <div class='btn-group'>
-                        <a href="{!! route('mosques.show', [$mosque->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
-                        <a href="{!! route('mosques.edit', [$mosque->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
-                        {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => 'return confirm("'.__('crud.are_you_sure').'")']) !!}
-                    </div>
-                    {!! Form::close() !!}
-                </td>
+                <td><a href="{!! route('mosques.show', [$mosque->id]) !!}">{!! $mosque->name !!}</a></td>
+                <td>{!! $mosque->image !!}</td>
+                <td>{!! $mosque->description !!}</td>
+                <td>{!! $mosque->address !!}</td>
+                <td>{!! $mosque->website !!}</td>
+                <td>{!! $mosque->prefectures !!}</td>
+                <td>{!! $mosque->city !!}</td>
+                <td>{!! $mosque->status !!}</td>
             </tr>
         @endforeach
         </tbody>
