056e2e96976767580bf59ec5f5bf7ec43d5ce993
Ahmad Syamim <ahmad.syamim@gmail.com>
2019-11-20 12:08:23 +0900

    remove unused function (Mosque & Event)

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -36,34 +36,6 @@ class EventController extends AppBaseController
     }
 
     /**
-     * Show the form for creating a new Event.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        return view('events.create');
-    }
-
-    /**
-     * Store a newly created Event in storage.
-     *
-     * @param CreateEventRequest $request
-     *
-     * @return Response
-     */
-    public function store(CreateEventRequest $request)
-    {
-        $input = $request->all();
-
-        $event = $this->eventRepository->create($input);
-
-        Flash::success(__('messages.saved', ['model' => __('models/events.singular')]));
-
-        return redirect(route('events.index'));
-    }
-
-    /**
      * Display the specified Event.
      *
      * @param int $id
@@ -81,76 +53,5 @@ class EventController extends AppBaseController
         }
 
         return view('events.show')->with('event', $event);
-    }
-
-    /**
-     * Show the form for editing the specified Event.
-     *
-     * @param int $id
-     *
-     * @return Response
-     */
-    public function edit($id)
-    {
-        $event = $this->eventRepository->find($id);
-
-        if (empty($event)) {
-            Flash::error(__('messages.not_found', ['model' => __('models/events.singular')]));
-
-            return redirect(route('events.index'));
-        }
-
-        return view('events.edit')->with('event', $event);
-    }
-
-    /**
-     * Update the specified Event in storage.
-     *
-     * @param int $id
-     * @param UpdateEventRequest $request
-     *
-     * @return Response
-     */
-    public function update($id, UpdateEventRequest $request)
-    {
-        $event = $this->eventRepository->find($id);
-
-        if (empty($event)) {
-            Flash::error(__('messages.not_found', ['model' => __('models/events.singular')]));
-
-            return redirect(route('events.index'));
-        }
-
-        $event = $this->eventRepository->update($request->all(), $id);
-
-        Flash::success(__('messages.updated', ['model' => __('models/events.singular')]));
-
-        return redirect(route('events.index'));
-    }
-
-    /**
-     * Remove the specified Event from storage.
-     *
-     * @param int $id
-     *
-     * @throws \Exception
-     *
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        $event = $this->eventRepository->find($id);
-
-        if (empty($event)) {
-            Flash::error(__('messages.not_found', ['model' => __('models/events.singular')]));
-
-            return redirect(route('events.index'));
-        }
-
-        $this->eventRepository->delete($id);
-
-        Flash::success(__('messages.deleted', ['model' => __('models/events.singular')]));
-
-        return redirect(route('events.index'));
     }
 }
